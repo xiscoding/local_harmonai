189262d595e27d2727e46699424f9e0d64f909eb
xdoestech <xdoestech@gmail.com>
2023-01-04 09:11:09 -0500

    Add files via upload

--- finetune.py
+++ finetune.py
@@ -1,14 +1,15 @@
 #@markdown Name for the finetune project, used as the W&B project name, as well as the directory for the saved checkpoints
-NAME="rap-song-finetune" #@param {type:"string"}
+NAME="ENTER NAME FOR WANB" #@param {type:"string"}
 
+#/home/xdoestech/harmonai/audio_out/finetune/44khz/run4_precision16/demo_00229001_synthesis_364_802714.wav
 #@markdown Path to the directory of audio data to use for fine-tuning
-TRAINING_DIR="/home/xdoestech/harmonai/init_audio" #@param {type:"string"}
+TRAINING_DIR="ENTER PATH" #@param {type:"string"}
 
 #@markdown Path to the checkpoint to fine-tune
-CKPT_PATH="/home/xdoestech/harmonai/checkpoints/maestro-uncond-150k.ckpt" #@param {type:"string"}
+CKPT_PATH="ENTER PATH" #@param {type:"string"}
 
 #@markdown Directory path for saving the fine-tuned outputs
-OUTPUT_DIR="/home/xdoestech/harmonai/models/finetune" #@param {type:"string"}
+OUTPUT_DIR="ENTER PATH " #@param {type:"string"}
 
 #@markdown Number of training steps between demos
 DEMO_EVERY=250 #@param {type:"number"}
@@ -16,11 +17,22 @@ DEMO_EVERY=250 #@param {type:"number"}
 #@markdown Number of training steps between saving model checkpoints
 CHECKPOINT_EVERY=500 #@param {type:"number"}
 
+####FILE SIZE CALCULATION
+# ((bitspersample(16-bit or 24-bit)*
+# samplespersec(44.1KHz-48KHz)*
+# no.of channels*
+# duration(no.of sec the music played)
+
 #@markdown Sample rate to train at
-SAMPLE_RATE=48000 #@param {type:"number"}
+# glitch: 48k
+# jmann: 48k
+# maestro: 16k
+# unlocked: 16k
+# honk: 16k
+SAMPLE_RATE=48100 #@param {type:"number"}
 
 #@markdown Number of audio samples per training sample
-SAMPLE_SIZE=(65536)#@param {type:"number"}
+SAMPLE_SIZE=(8192*16)#@param {type:"number"}
 #NOTE must be a multiple of 8192
 ##8192 * 8 = 65536
 ##8192 * 4 = 32768
@@ -40,8 +52,11 @@ BATCH_SIZE=1 #@param {type:"number"}
 #@markdown Effective batch size is BATCH_SIZE * ACCUM_BATCHES.
 #@markdown
 #@markdown Also increases the time between demos and saved checkpoints
-ACCUM_BATCHES=4 #@param {type:"number"}
+ACCUM_BATCHES=1 #@param {type:"number"}
 
+#XDTEDIT_1422
+#cuda device number to use
+DEVICE_NUM = 0
 random_crop_str = f"--random-crop True" if RANDOM_CROP else ""
 
 # Escape spaces in paths
@@ -60,10 +75,30 @@ os.system(f"python3 sample-generator/train_uncond.py --ckpt-path {CKPT_PATH}\
                                                           --batch-size {BATCH_SIZE}\
                                                           --demo-every {DEMO_EVERY}\
                                                           --checkpoint-every {CHECKPOINT_EVERY}\
-                                                          --num-workers 3\
+                                                          --num-workers 8\
                                                           --num-gpus 1\
                                                           {random_crop_str}\
+                                                          --device_num = {DEVICE_NUM}\
                                                           --save-path {OUTPUT_DIR}")
 
 
-#Make sure to change the OUTPUT_DIR in train_uncond.py
+# samplesize:65536
+# numworkers:4
+# batchsize:2
+# accumbatches:4
+## vram: 10415MiB
+
+# samplesize:65536-8192
+# numworkers:4
+# batchsize:2
+# accumbatches:4
+## vram: 9943MiB - 11649
+
+# samplesize:65536-8192*2
+# numworkers:4
+# batchsize:2
+# accumbatches:4
+## vram: 
+
+#NOTE: ValueError: num_samples should be a positive integer value, but got num_samples=0
+    #WRONG TRAINING PATH
---
+++ site_packages/prefigure.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+__author__ = 'S.H. Hawley'
+
+"""
+Routines for easily keeping track of & archiving run configurations.
+Supports config (.ini) files, pulling previous configs from WandB, 
+and overrides with command-line options.
+"""
+
+from pathlib import Path
+from ast import literal_eval 
+import argparse
+import configparser
+import wandb
+import sys
+import copy
+import distutils
+
+DEFAULTS_FILE = '/home/xdoestech/harmonai/sample-generator/defaults.ini'
+
+def arg_eval(value):
+    "this just packages some type checking for parsing args"
+    try: 
+        val = literal_eval(str(value))
+    except (SyntaxError, ValueError, AssertionError):
+        val = value
+    return val
+
+
+def read_defaults(defaults_file=DEFAULTS_FILE):
+    "read the defaults file, setup defaults dict"
+    p = argparse.ArgumentParser(add_help=False)
+    p.add_argument('--config-file', required=False, default=defaults_file,
+        help='name of local configuration (.ini) file')
+    config_file = p.parse_known_args()[0].config_file
+    configp = configparser.ConfigParser()
+    configp.optionxform = str                 # don't change uppercase to lowercase
+    configp.read(config_file)
+    defaults = dict(configp.items('DEFAULTS'))
+    with open(config_file) as f:
+        defaults_text = f.readlines()
+    return defaults, defaults_text
+
+
+def setup_args(defaults, defaults_text='',):
+    """combine defaults from .ini file and add parseargs arguments, 
+        with help pull from .ini"""
+    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)  
+    p.add_argument('--config-file', required=False, default=DEFAULTS_FILE, #added so it appears on -h list
+        help='name of local configuration (.ini) file')
+    p.add_argument('--wandb-config', required=False,  
+                   help='wandb url to pull config from')
+    #XDTEDIT_1422
+    p.add_argument('--device_num', required=False, default=0, help='cuda device number to use')
+
+    # add other command-line args using defaults .ini file
+    for key, value in defaults.items():
+        if (key in ['wandb_config','config_file']): break
+        help = ""
+        for i in range(len(defaults_text)):  # get the help string from defaults_text
+            if key in defaults_text[i]:
+                help = defaults_text[i-1].replace('# ','')
+        argname = '--'+key.replace('_','-')
+        val = Path(value) if ((type(value) == str) and ('_dir' in value)) else arg_eval(value)
+        val_type = type(val)
+        #print(f"argname: {argname}, val: {val}, val_type: {val_type}")
+        if val is None: # None is weird in Python
+            p.add_argument(argname, type=str, nargs='?', const=None, default=None, help=help)
+        elif (val_type != type(True)):   # gotta handle boolean values specially when dealing with argparse
+            p.add_argument(argname, default=val, type=val_type, help=help)
+        else: # normal string/int/etc
+            #val_type = bool(distutils.util.strtobool(val))
+            p.add_argument(argname, type=val_type, nargs='?', const=True, default=False, help=help)
+
+
+    args = p.parse_args() 
+        
+    return args
+    
+
+def pull_wandb_config(wandb_config, defaults):
+    """overwrites parts of defaults using wandb config info 
+       wandb_config is the url of one of your runs"""
+    api = wandb.Api()  # might get prompted for api key login the first time
+    splits = wandb_config.split('/')
+    entity, project, run_id = splits[3], splits[4], splits[-1].split('?')[0]
+    run = api.run(f"{entity}/{project}/{run_id}")
+    for key, value in run.config.items():
+        if 'OMITTED' != value: defaults[key] = arg_eval(value)
+    return defaults
+
+
+
+def push_wandb_config(wandb_logger, args, omit=[]): 
+    """
+    save config to wandb (for possible retrieval later)
+    Omit: list of args you don't want pushed to wandb; will push an empty string for these
+    """
+    if hasattr(wandb_logger.experiment.config, 'update'): #On multi-GPU runs, only process rank 0 has this attribute!
+        copy_args = copy.deepcopy(args)
+        for var_str in omit:  # don't push certain reserved settings to wandb
+            if hasattr(copy_args, var_str):
+                setattr(copy_args, var_str, 'OMITTED')
+        wandb_logger.experiment.config.update(copy_args)
+
+
+def get_all_args(defaults_file=DEFAULTS_FILE):
+    " Config setup."
+    #   1. Default settings are in defaults ini (or some other config) file
+    defaults, defaults_text = read_defaults(defaults_file=defaults_file)
+    args = setup_args(defaults, defaults_text=defaults_text)  
+
+    #   2. if --wandb-config is given, pull config from wandb to override defaults
+    if args.wandb_config is not None:
+        defaults = pull_wandb_config(args.wandb_config, defaults) # 2.
+
+    #   3. Any new command-line arguments override whatever was set earlier
+    args = setup_args(defaults, defaults_text=defaults_text) # 3. this time cmd-line overrides what's there
+
+    return args
+
+
+if __name__ == '__main__':
+    # quick test
+    args = get_all_args(defaults_file='../examples/defaults.ini')
+    print("args = ",args)
